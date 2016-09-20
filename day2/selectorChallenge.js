fd7508d1e5f93b5a23155dca088d6489dfbfbde1
Riley Osborne <mitchell.osborne@interapt.hq>
2016-09-20 09:46:56 -0400

    jQuery Selector Challenge

--- day2/selectorChallenge.js
+++ day2/selectorChallenge.js
@@ -1,3 +1,11 @@
-$("#paragraph").html("okay then");
+$(document).ready(function(){
+    $("#paragraph").html("okay then");
+});
+
+$(document).ready(function(){
 $("h3").css('color', 'red');
+});
+
+$(document).ready(function(){
 $("li").css('background-color', 'yellow');
+});
